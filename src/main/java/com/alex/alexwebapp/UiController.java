1827c5f1ddb720216f3e4bcd52d9bb1e7ea6e8ef
alex <alextjp>
2016-08-20 13:10:34 +0900

    add profile specific properties

--- src/main/java/com/alex/alexwebapp/UiController.java
+++ src/main/java/com/alex/alexwebapp/UiController.java
@@ -6,15 +6,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class UiController {
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	private static final Logger logger = LoggerFactory.getLogger(UiController.class);
+
+	@Autowired
+	private Environment environment;
+
+	@Value("${aproperty}")
+	String property;
+
+	@Value("${aproperty2}")
+	String property2;
+
 	@RequestMapping("hello")
 	public String hello(Model model) {
 		model.addAttribute("hello", jdbcTemplate.queryForObject("SELECT col FROM random_table WHERE id = 1", String.class));
+		logger.info(property);
+		logger.info(property2);
+		logger.info("{} {}", environment.getActiveProfiles()[0], environment.getActiveProfiles().length);
 		return "hello";
 	}
 }
