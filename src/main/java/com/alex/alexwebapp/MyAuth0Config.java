33553ad172905f340c476d46af2076488c686136
alex <alextjp@users.noreply.github.com>
2016-09-03 19:06:24 +0900

    fix stuff

--- src/main/java/com/alex/alexwebapp/MyAuth0Config.java
+++ src/main/java/com/alex/alexwebapp/MyAuth0Config.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
@@ -16,4 +17,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 public class MyAuth0Config extends Auth0Config {
 
+    @Override
+    protected void authorizeRequests(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(securedRoute).authenticated()
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin().loginPage("/login");
+    }
 }
--- src/main/java/com/alex/alexwebapp/WebMvcConfig.java
+++ src/main/java/com/alex/alexwebapp/WebMvcConfig.java
@@ -10,10 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.io.FileNotFoundException;
 
@@ -23,6 +20,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	HandlerInterceptor myHandlerInterceptorAdapter;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
